abd7810e7e63e66fcffcc551bbba776e261e8dea
Judah George <georgejudah5@gmail.com>
2023-02-10 00:50:52 +0530

    completed go routines

--- channels/main.go
+++ channels/main.go
@@ -4,6 +4,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -16,11 +17,14 @@ func main() {
 		"http://amazon.com",
 		"http://golang.org",
 	}
+	//creating a new channel
+	c := make(chan string)
+
 	for _, link := range links {
 		// By default when you run a program, we create a single go Routine
 		// checkLink(link)
 		//child routines are not getting respect as that of the main routine
-		go checkLink(link)
+		go checkLink(link, c)
 
 		//one core
 		// creates each go routine for each iteration and run the function with it
@@ -29,15 +33,29 @@ func main() {
 
 		//If we change the confi to run in multiple cores, then each go routine will run in each core
 	}
+
+	//infinite loop
+
+	for l := range c {
+		// go checkLink(l, c)
+		//anonymous function for sleep time - Function literals
+		go func(link string) {
+			time.Sleep(5 * time.Second)
+			checkLink(link, c)
+		}(l)
+		//blocking code which when gets a data, exits the code - passing this channel value is a blocking operation
+	}
+
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 
 	if err != nil {
 		fmt.Println(link + "might be down")
+		c <- link
 		return
 	}
-
+	c <- link
 	fmt.Println(link + " is up!")
 }
